6aa9480da215512e2da0282de308f116549580fb
mustafaerk <telemak33@gmail.com>
2021-07-17 23:11:21 +0300

    Route type change
    BrowerRouter change to HashRouter for github deploy page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 import Logo from "../../logo.svg";
 
